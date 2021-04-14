aa89cea9fd373386060d81811d1e5d4ce9d7788a
aqiang999 <313121337+>
2021-04-14 21:28:32 +0800

    更新2

--- src/main/java/com/lol/controller/HelloController.java
+++ src/main/java/com/lol/controller/HelloController.java
@@ -10,26 +10,26 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class HelloController {
 
-    @RequestMapping({"/","/index.html"})
-    public String index(){
-        return "dashboard";
-    }
+  @RequestMapping({"/", "/index.html"})
+  public String index() {
+    return "dashboard";
+  }
 
   @ResponseBody
   @RequestMapping("/hello")
-  public  String hello(@RequestParam("user") String user){
+  public String hello(@RequestParam("user") String user) {
 
     return "Hello World";
   }
 
   //查出用户数据，在页面展示
   @RequestMapping("/success")
-  public String success(Map<String,Object> map){
-    map.put("hello","<h1>你好</h1>");
-    map.put("users", Arrays.asList("zhangsan","lisi","wangwu"));
+  public String success(Map<String, Object> map) {
+    map.put("hello", "<h1>你好</h1>");
+    map.put("gg", "22");
+    map.put("users", Arrays.asList("zhangsan", "lisi", "wangwu"));
     return "success";
   }
-
 
 
 }
